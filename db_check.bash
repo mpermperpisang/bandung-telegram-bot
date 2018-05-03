e49d5a5534825a4ceddf1d6b34427c4d5ce786d0
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2018-05-04 01:45:22 +0700

    delete file

--- db_check.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ./callruby.rb
-sshpass -p "yourpassword" ssh username@staging <<ENDSSH
-  mysql -u"root" -p"yourpassword" -D"yourdatabase" -e"select day from yourtabel where name='$name'" > bot/callruby.rb
-ENDSSH
--- db_list.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ./callruby.rb
-sshpass -p "yourpassword" ssh username@staging <<ENDSSH
-  mysql -u"root" -p"yourpassword" -D"yourdatabase" -e"SELECT name FROM yourtabel where day='$day' and status='belum'; update yourtabel set status='belum' where day<>'$day'" > bot/callruby.rb
-ENDSSH
