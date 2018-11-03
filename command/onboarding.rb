6936f7886b245420b49b0f6b88e7189fa0b41cfc
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2018-11-03 22:10:27 +0700

    private message onboarding

---
+++ command/onboarding.rb
@@ -0,0 +1,34 @@
+def onboarding(message, bot)
+  @db = Connection.new
+  
+  @txt = message.text
+  
+  @array = []
+
+  check_member = @db.check_onboarding("@#{message.from.username}")
+  @check = check_member.size.zero? ? nil : check_member.first['on_flag']
+
+  if @txt[/^(.*(?=Nama panggilan :).*[a-zA-Z.,&()]\n.*(?=Job title :).*[a-zA-Z.,&()]\n.*(?=Squad :).*[a-zA-Z.,&()]\n(.*(?=Pekerjaan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan atau Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n).*(?=Status :).*[a-zA-Z.,&()]\n.*(?=Hobi :).*[a-zA-Z.,&()]\n.*(?=Motto :).*[a-zA-Z.,&()])$/] != nil
+     @db.done_onboarding(message.from.username)
+     bot.api.send_message(chat_id: ENV['ID_REMINDER'], text: msg_welcome_new_member(message.from.first_name, message.from.username), parse_mode: 'HTML') if @check == "false"
+     bot.api.send_message(chat_id: ENV['ID_REMINDER'], text: "Berikut perkenalan dari Kak @#{message.from.username}\n#{@txt}", parse_mode: 'HTML') if @check == "false"
+     bot.api.send_message(chat_id: message.from.id, text: input_buka_bandung) if @check == "false"
+     @chat.delete(bot, message.chat.id, message.message_id) if @check == "true" or @check.nil?
+  end
+
+  @array.push('Nama panggilan') if @txt[/^(.*(?=Nama panggilan :).*[a-zA-Z.,&()])$/].nil?
+  @array.push('Job title') if @txt[/^(.*(?=Job title :).*[a-zA-Z.,&()])$/].nil?
+  @array.push('Squad') if @txt[/^(.*(?=Squad :).*[a-zA-Z.,&()])$/].nil?
+  @array.push('Pekerjaan atau Pendidikan terakhir') if @txt[/^(.*(?=Pekerjaan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan atau Pendidikan terakhir :).*[a-zA-Z0-9.,&()])$/].nil?
+  @array.push('Status') if @txt[/^(.*(?=Status :).*[a-zA-Z.,&()])$/].nil?
+  @array.push('Hobi') if @txt[/^(.*(?=Hobi :).*[a-zA-Z.,&()])$/].nil?
+  @array.push('Motto') if @txt[/^(.*(?=Motto :).*[a-zA-Z.,&()])$/].nil?
+
+  check_member = @db.check_onboarding("@#{message.from.username}")
+  @check = check_member.size.zero? ? nil : check_member.first['on_flag']
+
+  @list = @array.to_s.gsub('", "', ", ").delete('["').delete('"]')
+  unless @array.empty? && @check.nil?
+    bot.api.send_message(chat_id: message.from.id, text: msg_onboarding(message.from.username, @list), parse_mode: 'HTML') if @check == "false"
+  end
+end
--- run/bandung/snack.rb
+++ run/bandung/snack.rb
@@ -59,35 +59,9 @@ Telegram::Bot::Client.run(@token) do |bot|
         else
           snack_private(@token, message.from.id, bot, message, @txt)
         end
+        
+        onboarding(message, bot)
       end
-=begin
-      @array = []
-
-      check_member = @db.check_onboarding("@#{message.from.username}")
-      @check = check_member.size.zero? ? nil : check_member.first['on_flag']
-
-      if @txt[/^(.*(?=Nama panggilan :).*[a-zA-Z.,&()]\n.*(?=Job title :).*[a-zA-Z.,&()]\n.*(?=Squad :).*[a-zA-Z.,&()]\n(.*(?=Pekerjaan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan atau Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n).*(?=Status :).*[a-zA-Z.,&()]\n.*(?=Hobi :).*[a-zA-Z.,&()]\n.*(?=Motto :).*[a-zA-Z.,&()])$/] != nil
-        @db.done_onboarding(message.from.username)
-        bot.api.send_message(chat_id: message.chat.id, text: msg_welcome_new_member(message.from.first_name, message.from.username), parse_mode: 'HTML') if @check == "false"
-        @chat.delete(bot, message.chat.id, message.message_id) if @check == "true" or @check.nil?
-      end
-
-      @array.push('Nama panggilan') if @txt[/^(.*(?=Nama panggilan :).*[a-zA-Z.,&()])$/].nil?
-      @array.push('Job title') if @txt[/^(.*(?=Job title :).*[a-zA-Z.,&()])$/].nil?
-      @array.push('Squad') if @txt[/^(.*(?=Squad :).*[a-zA-Z.,&()])$/].nil?
-      @array.push('Pekerjaan atau Pendidikan terakhir') if @txt[/^(.*(?=Pekerjaan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pendidikan terakhir :).*[a-zA-Z0-9.,&()]\n|.*(?=Pekerjaan atau Pendidikan terakhir :).*[a-zA-Z0-9.,&()])$/].nil?
-      @array.push('Status') if @txt[/^(.*(?=Status :).*[a-zA-Z.,&()])$/].nil?
-      @array.push('Hobi') if @txt[/^(.*(?=Hobi :).*[a-zA-Z.,&()])$/].nil?
-      @array.push('Motto') if @txt[/^(.*(?=Motto :).*[a-zA-Z.,&()])$/].nil?
-
-      check_member = @db.check_onboarding("@#{message.from.username}")
-      @check = check_member.size.zero? ? nil : check_member.first['on_flag']
-
-      @list = @array.to_s.gsub('", "', ", ").delete('["').delete('"]')
-      unless @array.empty? && @check.nil?
-        bot.api.send_message(chat_id: message.chat.id, text: msg_onboarding(message.from.username, @list), parse_mode: 'HTML') if @check == "false"
-      end
-=end
     end
   rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
     puts e
