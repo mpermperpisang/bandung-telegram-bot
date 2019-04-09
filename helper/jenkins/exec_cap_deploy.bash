1946ee17a26ed4f7a4b0b5a2e6540e1041660fc0
Ferawati Hartanti Pratiwi <mpermperpisang@users.noreply.github.com>
2019-04-09 08:52:49 +0700

    Update exec_cap_deploy.bash

--- helper/jenkins/exec_cap_deploy.bash
+++ helper/jenkins/exec_cap_deploy.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/bukalapak/bandung-telegram-bot/require/ruby_cap.rb
-sshpass -p "bukalapak" ssh bukalapak@$ip_staging <<ENDSSH
+sshpass -p "password" ssh bukalapak@$ip_staging <<ENDSSH
   cd deploy
   printf "$staging_branch\nbukalapak\nbukalapak" | bundle exec cap staging $type &> ~/deploy/log/cap.log
 ENDSSH
