c1ac762c9efa91a963a33f15c01470e8e23052a6
Ferawati Hartanti Pratiwi <mpermperpisang@users.noreply.github.com>
2019-04-26 22:06:08 +0700

    Update exec_normalize.bash

--- helper/jenkins/exec_normalize.bash
+++ helper/jenkins/exec_normalize.bash
@@ -3,7 +3,7 @@ source ./require_ruby.rb
 
 echo "$month $time"
 
-sshpass -p "password" ssh bukalapak@$ip_staging <<ENDSSH
+sshpass -p "password" ssh user@$ip_staging <<ENDSSH
   echo "$date $time"
-  echo "bukalapak" | sudo -S date --set "$date $time"
+  echo "password" | sudo -S date --set "$date $time"
 ENDSSH
