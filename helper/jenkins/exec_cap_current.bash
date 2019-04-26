36afd3c4b539cec484f0f5496918d0e8fb34df2f
Ferawati Hartanti Pratiwi <mpermperpisang@users.noreply.github.com>
2019-04-26 22:04:25 +0700

    Update exec_cap_current.bash

--- helper/jenkins/exec_cap_current.bash
+++ helper/jenkins/exec_cap_current.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/bukalapak/bandung-telegram-bot/require/ruby_cap.rb
-sshpass -p "password" ssh bukalapak@$ip_staging <<ENDSSH
+sshpass -p "password" ssh user@$ip_staging <<ENDSSH
   cd current
-  printf "$staging_branch\nbukalapak\nbukalapak" | bundle exec cap staging $type &> ~/current/log/cap.log
+  printf "$staging_branch\nuser\npassword" | bundle exec cap staging $type &> ~/current/log/cap.log
 ENDSSH
